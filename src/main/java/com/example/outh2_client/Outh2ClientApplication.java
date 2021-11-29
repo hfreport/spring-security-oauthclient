912c1e990659b94c126c45dc7e08c138a8184510
hufeng <xxx>
2021-11-29 09:44:21 +0800

    first commit

---
+++ src/main/java/com/example/outh2_client/Outh2ClientApplication.java
@@ -0,0 +1,13 @@
+package com.example.outh2_client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Outh2ClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Outh2ClientApplication.class, args);
+    }
+
+}
